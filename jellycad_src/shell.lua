5e96c673fb7314e12a760e714af362ac1308c417
Jelatine <429572807@qq.com>
2025-12-26 16:58:39 +0800

    增加模组外壳

---
+++ jellycad_src/shell.lua
@@ -0,0 +1,63 @@
+--[[
+Copyright (c) 2025. Li Jianbin. All rights reserved.
+MIT License
+JellyCAD version v0.3.9
+Mockway Structure Motor Shell Model File
+--]]
+local config = require('config')
+
+shell = cylinder.new(config.r_shell_outer, config.h_shell)
+-- CAN插口槽
+shell:cut(box.new({ -config.w_can_plug / 2, -config.r_outer, config.z_plug },
+    { config.w_can_plug / 2, config.r_outer, config.z_plug_top }))
+-- TTL插口槽
+shell:cut(box.new({ -config.r_outer, -config.w_ttl_plug / 2, config.z_plug },
+    { 0, config.w_ttl_plug / 2, config.z_plug_top }))
+-- 标定孔
+shell:cut(cylinder.new(config.r_calib_slot, config.h_calib_slot):rx(90):y(config.r_outer))
+shell:cut(cylinder.new(config.r_calib_slot, config.h_calib_slot):rx(-90):y(-config.r_outer))
+-- 盖板固定孔
+for deg = 45, 360, 90 do
+    local rad = deg * math.pi / 180;
+    local len = (config.r_shell_outer + config.r_shell_inner) / 2
+    local x0 = len * math.sin(rad);
+    local y0 = len * math.cos(rad);
+    shell:cut(cylinder.new(config.r_m2_tapping, 4):rx(180):pos(x0, y0, config.h_shell))
+end
+-- 合并连接台
+local connecter = cylinder.new(config.r_shell_outer, config.r_shell_outer + config.h_flank):z(config.r_shell_outer):ry(90)
+connecter:cut(cylinder.new(config.r_shell_outer, config.h_shell))
+-- 标定孔
+connecter:cut(cylinder.new(config.r_calib_slot, config.h_calib_slot):rx(90)
+    :pos(config.r_outer + config.h_flank, config.r_outer, config.r_outer))
+connecter:cut(cylinder.new(config.r_calib_slot, config.h_calib_slot):rx(-90)
+    :pos(config.r_outer + config.h_flank, -config.r_outer, config.r_outer))
+shell:fuse(connecter)
+-- 切割法兰凹台
+shell:cut(cylinder.new(config.r_flank_groove, config.h_flank):ry(90):x(config.r_shell_outer):z(config.r_shell_outer))
+-- 法兰侧部M2螺丝孔
+shell:cut(cylinder.new(config.r_m2_hole, 2 * config.r_shell_outer):x(config.r_shell_outer + config.h_flank / 2));
+-- 切割内圈
+shell:cut(cylinder.new(config.r_shell_inner, config.h_shell):z(config.thickness))
+-- 切割法兰孔
+shell:cut(cylinder.new(config.r_shell_flank_hole, config.thickness))
+-- 圆形通线孔
+shell:cut(cylinder.new(config.r_wire_hole, config.r_shell_outer):z(config.r_shell_outer):ry(90))
+-- 方形通线槽
+shell:cut(box.new(config.w_wire_solt, config.l_wire_solt, config.r_outer):x(config.r_shell_inner):z(config.r_outer))
+
+for deg = 60, 360, 60 do
+    local rad = deg * math.pi / 180;
+    -- 电机外壳螺丝通孔
+    local x0 = config.r_screw_distribut * math.sin(rad);
+    local y0 = config.r_screw_distribut * math.cos(rad);
+    shell:cut(cylinder.new(config.r_m3_hole, config.thickness):pos(x0, y0, 0));
+    shell:cut(cone.new(config.r_m3_head, 0, config.r_m3_head):pos(x0, y0, 0));
+    -- 法兰固定热熔螺母孔
+    local y1 = config.r_flank_screw_pos * math.cos(rad);
+    local z1 = config.r_flank_screw_pos * math.sin(rad) + config.r_outer;
+    shell:cut(cylinder.new(config.r_m3d4_nut, config.r_shell_outer):ry(90):pos(0, y1, z1));
+end
+show(shell:color('turquoise'))
+shell:export_step('shell.step')
+return { model = shell:copy(), m = shell:copy():scale(1e-3) }
